a803f23436c93056660a1a00f2ca546d84efd448
Saurabh Bothra <saurabhbothra29@gmail.com>
2020-12-27 00:04:27 -0500

    initial setup

---
+++ src/com/algorithms/practice/demo.java
@@ -0,0 +1,10 @@
+package com.algorithms.practice;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
