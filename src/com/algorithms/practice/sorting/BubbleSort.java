2e81e7d134b88aa6a63346da2661ca9e3349bdcd
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-05-25 22:22:59 -0400

    bubble sort

--- src/com/algorithms/practice/sorting/BubbleSort.java
+++ src/com/algorithms/practice/sorting/BubbleSort.java
@@ -1,12 +1,35 @@
 package com.algorithms.practice.sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
-	
-	// 
+
+	// implement bubble sort algorithm.
+
+	// efficient solution.
+	public static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 1; i < n; i++) {
+			boolean flag = false;
+			for (int j = 0; j < n - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					flag = true;
+				}
+			}
+			if (!flag) {
+				break;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arr = { 2, 10, 8, 7 };
+		bubbleSort(arr);
+		System.out.println("The array after sorting is: " + Arrays.toString(arr));
 	}
 
 }
