c4acd5a7ccfa361b7a86434782d2889534e349ac
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-03 19:23:08 -0400

    matrix

---
+++ src/com/algorithms/practice/matrix/MatrixInSnakePattern.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.matrix;
+
+public class MatrixInSnakePattern {
+	
+	// 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
