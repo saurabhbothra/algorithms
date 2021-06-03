a39aa9b8a4ba4d2aae886209884c7dfe272afb2e
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-03 19:53:35 -0400

    matrix in snake pattern

--- src/com/algorithms/practice/matrix/MatrixInSnakePattern.java
+++ src/com/algorithms/practice/matrix/MatrixInSnakePattern.java
@@ -1,12 +1,30 @@
 package com.algorithms.practice.matrix;
 
 public class MatrixInSnakePattern {
-	
-	// 
+
+	// Given a matrix, our task is to print matrix in snake pattern.
+
+	// efficient solution.
+	public static void printMatrixInSnakePattern(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				if (i % 2 == 0) {
+					System.out.print(arr[i][j] + " ");
+				} else {
+					System.out.print(arr[i][arr[i].length - 1 - j] + " ");
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[][] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+		int[][] arr1 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+		System.out.println("The matrix in snake pattern is: ");
+		printMatrixInSnakePattern(arr);
+		System.out.println();
+		printMatrixInSnakePattern(arr1);
 	}
 
 }
