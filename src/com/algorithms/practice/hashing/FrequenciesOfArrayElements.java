768ed85665fb047c71bc012f4a56e4e706b34840
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-07 19:54:51 -0400

    frequencies of array elements

--- src/com/algorithms/practice/hashing/FrequenciesOfArrayElements.java
+++ src/com/algorithms/practice/hashing/FrequenciesOfArrayElements.java
@@ -1,17 +1,48 @@
 package com.algorithms.practice.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FrequenciesOfArrayElements {
 
 	// Given an array of integers, print frequencies of all elements in the array.
 
 	// naive solution.
 	public static void printFrequenciesNaive(int[] arr) {
-
+		for (int i = 0; i < arr.length; i++) {
+			boolean flag = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] == arr[i]) {
+					flag = true;
+					break;
+				}
+			}
+			if (flag) {
+				continue;
+			}
+			int count = 1;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] == arr[i]) {
+					count = count + 1;
+				}
+			}
+			System.out.println(arr[i] + " " + count);
+		}
 	}
 
 	// efficient solution.
 	public static void printFrequencies(int[] arr) {
-
+		Map<Integer, Integer> countMap = new HashMap<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (countMap.containsKey(arr[i])) {
+				countMap.put(arr[i], countMap.get(arr[i]) + 1);
+			} else {
+				countMap.put(arr[i], 1);
+			}
+		}
+		for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
 	}
 
 	public static void main(String[] args) {
