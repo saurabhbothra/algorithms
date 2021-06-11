4403bb544b9c714268b468acb6e47214bc339e0f
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-10 21:54:35 -0400

    node of a linked list

---
+++ src/com/algorithms/practice/linkedlist/Node.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.linkedlist;
+
+// This class represents a node in linked list.
+public class Node {
+	public int data;
+	public Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
