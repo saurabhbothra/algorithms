1beacae05d5bf32248cf374a9f22931d4542beb2
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-10 20:08:13 -0400

    linked list

---
+++ src/com/algorithms/practice/linkedlist/LinkedList.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.linkedlist;
+
+public class LinkedList {
+	
+	// Implement Linked List operations.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
