335d1ad8fc1a495a003531c4ae369632abb865ec
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-11 18:37:57 -0400

    node of a circular linked list

---
+++ src/com/algorithms/practice/linkedlist/CircularNode.java
@@ -0,0 +1,11 @@
+package com.algorithms.practice.linkedlist;
+
+//This class represents a node in circular linked list.
+public class CircularNode {
+	public int data;
+	public CircularNode next;
+	public CircularNode(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
