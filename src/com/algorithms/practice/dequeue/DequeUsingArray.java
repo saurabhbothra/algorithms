5a6c839aa24604975a9d161eba2f2683f932f258
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-23 17:10:15 -0400

    deque

---
+++ src/com/algorithms/practice/dequeue/DequeUsingArray.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.dequeue;
+
+public class DequeUsingArray {
+	
+	// Implement deque operations using array.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
