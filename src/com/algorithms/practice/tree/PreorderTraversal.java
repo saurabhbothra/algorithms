f064217e8e8b75ccbc46276faba080c74f4dab4b
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-06-29 19:46:46 -0400

    preorder traversal of a binart tree

---
+++ src/com/algorithms/practice/tree/PreorderTraversal.java
@@ -0,0 +1,17 @@
+package com.algorithms.practice.tree;
+
+public class PreorderTraversal {
+
+	// Implement preorder traversal of binary tree. (root -> left subtree -> right
+	// subtree)
+
+	// efficient solution.
+	public static void preorderTraversal(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.data + " ");
+		preorderTraversal(root.left);
+		preorderTraversal(root.right);
+	}
+}
