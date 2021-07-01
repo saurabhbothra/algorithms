5b3d917849304a0d58c2ebfe63da34f80ff455fc
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-07-01 15:18:20 -0400

    children sum property of a binary tree

---
+++ src/com/algorithms/practice/tree/ChildrenSumProperty.java
@@ -0,0 +1,36 @@
+package com.algorithms.practice.tree;
+
+public class ChildrenSumProperty {
+
+	// Children Sum Property is a property in which the sum of values of the left
+	// child and right child should be equal to the value of their node if both
+	// children are present. Else if only one child is present then the value of the
+	// child should be equal to its node value.
+
+	// write a function that takes the root node as a parameter and returns 1 if
+	// the Tree follows C.S.P. and 0 if the Tree does not follow C.S.P.
+
+	// recursive solution.
+	public static int isSumProperty(TreeNode root) {
+		if (root == null) {
+			return 1;
+		}
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+		int sum = 0;
+		if (root.left != null) {
+			sum += root.left.data;
+		}
+		if (root.right != null) {
+			sum += root.right.data;
+		}
+		int leftVal = isSumProperty(root.left);
+		int rightVal = isSumProperty(root.right);
+		if ((leftVal == 1 && rightVal == 1) && (sum == root.data)) {
+			return 1;
+		}
+		return 0;
+	}
+
+}
