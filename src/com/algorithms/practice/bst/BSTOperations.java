ab3624ca5d0306ab83dc909ba731f28f95e4ffaf
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-07-08 15:15:57 -0400

    binary search tree

---
+++ src/com/algorithms/practice/bst/BSTOperations.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.bst;
+
+public class BSTOperations {
+	
+	// Implement operations of binary search tree.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
