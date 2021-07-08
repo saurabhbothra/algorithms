2675df2a756c63b90bc35c419951e1560da51059
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-07-08 00:01:46 -0400

    inorder traversal iterative and recursive

--- src/com/algorithms/practice/tree/InorderTraversal.java
+++ src/com/algorithms/practice/tree/InorderTraversal.java
@@ -1,11 +1,15 @@
 package com.algorithms.practice.tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InorderTraversal {
 
 	// Implement inorder traversal of binary tree. (left subtree -> root -> right
 	// subtree)
 
-	// efficient solution.
+	// efficient recursive solution.
 	public static void inorderTraversal(TreeNode root) {
 		if (root == null) {
 			return;
@@ -13,6 +17,30 @@ public class InorderTraversal {
 		inorderTraversal(root.left);
 		System.out.print(root.data + " ");
 		inorderTraversal(root.right);
+	}
+
+	// efficient iterative solution.
+	public List<Integer> inorderTraversalIterative(TreeNode root) {
+		List<Integer> inorderList = new ArrayList<>();
+		if (root == null) {
+			return inorderList;
+		}
+		ArrayDeque<TreeNode> st = new ArrayDeque<>();
+		st.push(root);
+		TreeNode curr = root;
+		while (!st.isEmpty()) {
+			while (curr != null && curr.left != null) {
+				st.push(curr.left);
+				curr = curr.left;
+			}
+			TreeNode poppedNode = st.pop();
+			inorderList.add(poppedNode.data);
+			curr = poppedNode.right;
+			if (curr != null) {
+				st.push(curr);
+			}
+		}
+		return inorderList;
 	}
 
 }
