33ed58d3ed2dace13c72658b23ad944ff9d9131a
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-07-30 20:36:08 -0400

    greedy

---
+++ src/com/algorithms/practice/greedy/ActivitySelectionProblem.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.greedy;
+
+public class ActivitySelectionProblem {
+	
+	// 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
