c4905c34d922d044b918395f2bed5cc75c9f6a40
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-08-07 23:00:26 -0400

    kmp algorithm for pattern searching problem

--- src/com/algorithms/practice/strings/KMPAlgorithm.java
+++ src/com/algorithms/practice/strings/KMPAlgorithm.java
@@ -7,7 +7,32 @@ public class KMPAlgorithm {
 
 	// efficient implementation using kmp algorithm.
 	public static void patternSearching(String pat, String text) {
-		
+		int n = text.length();
+		int m = pat.length();
+		int count = 0;
+		int i = 0;
+		int j = 0;
+		int[] lps = ConstructLPSArray.constructLps(pat);
+		while (i < n) {
+			if (text.charAt(i) == pat.charAt(j)) {
+				i++;
+				j++;
+			}
+			if (j == m) {
+				count++;
+				System.out.print(i - j + " ");
+				j = lps[j - 1];
+			} else if (i < n && text.charAt(i) != pat.charAt(j)) {
+				if (j == 0) {
+					i++;
+				} else {
+					j = lps[j - 1];
+				}
+			}
+		}
+		if (count == 0) {
+			System.out.print("Pattern Not Found");
+		}
 	}
 
 	public static void main(String[] args) {
