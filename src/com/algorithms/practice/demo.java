d7fe0c0a5f9c1b1673e6d55697eb4d035f77ddb9
Saurabh Bothra <saurabhbothra29@gmail.com>
2021-08-14 16:23:26 -0400

    demo

--- src/com/algorithms/practice/demo.java
+++ src/com/algorithms/practice/demo.java
@@ -1,10 +1,8 @@
 package com.algorithms.practice;
 
 public class demo {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
-
 }
