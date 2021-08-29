70ed630a6dee17b4b4e3321a897249ad6d4eca8c
saurabhbothra <saurabhbothra29@gmail.com>
2021-08-28 22:12:12 -0400

    count distict rows in a binary matrix

--- src/com/algorithms/practice/trie/CountDistinctRowsInABinaryMatrix.java
+++ src/com/algorithms/practice/trie/CountDistinctRowsInABinaryMatrix.java
@@ -2,7 +2,6 @@ package com.algorithms.practice.trie;
 
 class BTrieNode {
 	BTrieNode[] childs = new BTrieNode[2];
-	boolean isEnd;
 }
 
 public class CountDistinctRowsInABinaryMatrix {
@@ -10,52 +9,35 @@ public class CountDistinctRowsInABinaryMatrix {
 	// Given a binary matrix, count distinct rows in it.
 
 	private BTrieNode root;
-	public int count;
 
 	public CountDistinctRowsInABinaryMatrix() {
 		this.root = new BTrieNode();
-		this.count = 0;
 	}
 
-	public void insert(int[] row) {
-		if (row == null || row.length == 0) {
-			return;
-		}
+	public boolean insert(int[] row) {
 		BTrieNode curr = this.root;
+		boolean isDistinct = false;
 		for (int i = 0; i < row.length; i++) {
 			if (curr.childs[row[i]] == null) {
 				curr.childs[row[i]] = new BTrieNode();
+				isDistinct = true;
 			}
 			curr = curr.childs[row[i]];
 		}
-		curr.isEnd = true;
-	}
-
-	public int countUniqueRows() {
-		countRec(this.root);
-		return count;
-	}
-
-	public void countRec(BTrieNode curr) {
-		if (curr.isEnd) {
-			this.count++;
-			return;
-		}
-		for (int i = 0; i < curr.childs.length; i++) {
-			if (curr.childs[i] != null) {
-				countRec(curr.childs[i]);
-			}
-		}
+		return isDistinct;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CountDistinctRowsInABinaryMatrix t = new CountDistinctRowsInABinaryMatrix();
 		int[][] matrix = { { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 1, 1, 0, 1 } };
+		int count = 0;
 		for (int i = 0; i < matrix.length; i++) {
-			t.insert(matrix[i]);
+			if (t.insert(matrix[i])) {
+				count++;
+			}
 		}
-		System.out.println("The number of distinct rows in the matrix is: " + t.countUniqueRows());
+		System.out.println("The number of distinct rows in the matrix is: " + count);
 	}
 
 }
