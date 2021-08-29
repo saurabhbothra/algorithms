29268e3ee77078702997cce4158fb1709e1255fb
saurabhbothra <saurabhbothra29@gmail.com>
2021-08-28 22:23:41 -0400

    segment and binary indexed trees

---
+++ src/com/algorithms/practice/segmentandbinaryindexedtree/ConstructingSegmentTree.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.segmentandbinaryindexedtree;
+
+public class ConstructingSegmentTree {
+	
+	// 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
