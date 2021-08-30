aa726567df2e0068bf2c7bc8e818ff4fd3fb9035
saurabhbothra <saurabhbothra29@gmail.com>
2021-08-30 01:41:25 -0400

    bit manipulation

---
+++ src/com/algorithms/practice/bitmagic/CheckKthBitIsSetOrNot.java
@@ -0,0 +1,12 @@
+package com.algorithms.practice.bitmagic;
+
+public class CheckKthBitIsSetOrNot {
+	
+	// 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
