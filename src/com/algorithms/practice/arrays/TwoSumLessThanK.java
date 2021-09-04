c875c54a362a7ebeb4ffa00b8315c151c8edfb7f
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-04 19:34:42 -0400

    two sum less than k

--- src/com/algorithms/practice/arrays/TwoSumLessThanK.java
+++ src/com/algorithms/practice/arrays/TwoSumLessThanK.java
@@ -8,7 +8,7 @@ public class TwoSumLessThanK {
 	// that there exists i < j with nums[i] + nums[j] = sum and sum < k. If no i, j
 	// exist satisfying this equation, return -1.
 
-	// efficient solution.
+	// efficient solution, using two pointers and sorting.
 	public static int twoSumLessThanK(int[] nums, int k) {
 		Arrays.sort(nums);
 		int i = 0;
@@ -25,11 +25,42 @@ public class TwoSumLessThanK {
 		return result;
 	}
 
+	// efficient solution, using binary search.
+	public static int twoSumLessThanKBinary(int[] nums, int k) {
+		int result = -1;
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length; i++) {
+			int complement = k - nums[i];
+			int subResult = binarySearch(nums, i, complement);
+			result = Math.max(result, subResult);
+		}
+		return result;
+	}
+
+	public static int binarySearch(int[] nums, int i, int x) {
+		int start = i + 1;
+		int end = nums.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (nums[mid] < x) {
+				if (mid != nums.length - 1 && nums[mid + 1] < x) {
+					start = mid + 1;
+				} else {
+					return nums[i] + nums[mid];
+				}
+			} else {
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = { 34, 23, 1, 24, 75, 33, 54, 8 };
 		int k = 60;
 		System.out.println("Maximum sum less than k is: " + twoSumLessThanK(nums, k));
+		System.out.println("Maximum sum less than k is: " + twoSumLessThanKBinary(nums, k));
 	}
 
 }
