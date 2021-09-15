16e93e0ca12a33ceafc39e0d73373569c261ff29
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-15 17:44:50 -0400

    maximum sum subaaray

--- src/com/algorithms/practice/arrays/MaximumSubarraySum.java
+++ src/com/algorithms/practice/arrays/MaximumSubarraySum.java
@@ -20,6 +20,36 @@ public class MaximumSubarraySum {
 		return maxSum;
 	}
 
+	// solution using divide and conquer.
+	public int maxSubArray(int[] nums) {
+		return sumRec(nums, 0, nums.length - 1);
+	}
+
+	private int sumRec(int[] nums, int left, int right) {
+		if (left > right) {
+			return Integer.MIN_VALUE;
+		}
+		int curr = 0;
+		int leftBestSum = 0;
+		int rightBestSum = 0;
+		int mid = left + (right - left) / 2;
+		for (int i = mid - 1; i >= left; i--) {
+			curr += nums[i];
+			leftBestSum = Math.max(leftBestSum, curr);
+		}
+		curr = 0;
+		for (int i = mid + 1; i <= right; i++) {
+			curr += nums[i];
+			rightBestSum = Math.max(rightBestSum, curr);
+		}
+
+		int bestSum = leftBestSum + rightBestSum + nums[mid];
+
+		int leftSum = sumRec(nums, left, mid - 1);
+		int rightSum = sumRec(nums, mid + 1, right);
+		return Math.max(bestSum, Math.max(leftSum, rightSum));
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = { 2, 3, -8, 7, -1, 2, 3 };
