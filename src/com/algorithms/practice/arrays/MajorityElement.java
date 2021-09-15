7c3e60db80711f8dcd548ec1819a169d8fbc95a1
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-15 17:32:01 -0400

    majority element

--- src/com/algorithms/practice/arrays/MajorityElement.java
+++ src/com/algorithms/practice/arrays/MajorityElement.java
@@ -8,6 +8,37 @@ public class MajorityElement {
 	// may assume that the majority element always exists in the array.
 	// 1 <= n
 
+	// solution using divide and conquer.
+	public static int majorityElementDivide(int[] nums) {
+		return mergeSort(nums, 0, nums.length - 1);
+	}
+
+	private static int mergeSort(int[] nums, int start, int end) {
+		if (start == end) {
+			return nums[start];
+		}
+		int mid = start + (end - start) / 2;
+		int left = mergeSort(nums, start, mid);
+		int right = mergeSort(nums, mid + 1, end);
+		if (left == right) {
+			return left;
+		}
+		int leftCount = fetchCount(nums, left, start, end);
+		int rightCount = fetchCount(nums, right, start, end);
+		return leftCount > rightCount ? left : right;
+
+	}
+
+	private static int fetchCount(int[] nums, int element, int start, int end) {
+		int count = 0;
+		for (int i = start; i <= end; i++) {
+			if (nums[i] == element) {
+				count++;
+			}
+		}
+		return count;
+	}
+
 	// efficient solution, Boyer-Moore Voting Algorithm
 	public static int majorityElement(int[] nums) {
 		int result = 0;
@@ -30,6 +61,7 @@ public class MajorityElement {
 		// TODO Auto-generated method stub
 		int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
 		System.out.println("The majority element in the given array is: " + majorityElement(nums));
+		System.out.println("The majority element in the given array is: " + majorityElementDivide(nums));
 	}
 
 }
--- src/com/algorithms/practice/demo.java
+++ src/com/algorithms/practice/demo.java
@@ -1,67 +1,27 @@
 package com.algorithms.practice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class demo {
 
-	public static int getNumberOfDigits(String s) {
-		Map<Character, Integer> hmap = new HashMap<>();
-		Map<Character, Integer> leftMap = new HashMap<>();
-		for (int i = 0; i < s.length(); i++) {
-			hmap.put(s.charAt(i), hmap.getOrDefault(s.charAt(i), 0) + 1);
-		}
-		int count = 0;
-		for (int i = 0; i < s.length() - 1; i++) {
-			leftMap.put(s.charAt(i), leftMap.getOrDefault(s.charAt(i), 0) + 1);
-			hmap.put(s.charAt(i), hmap.get(s.charAt(i)) - 1);
-			if (validate(hmap) && validate(leftMap)) {
+	public static void findFrequencies(int[] arr) {
+		int i = 0;
+		while(i < arr.length) {
+			int count = 1;
+			int j = i + 1;
+			while(j < arr.length && arr[j] == arr[i]) {
 				count++;
+				j++;
 			}
+			System.out.println(arr[i] + " " + count);
+			i = j;
 		}
-
-		return count;
 	}
 
-	public static boolean validate(Map<Character, Integer> hmap) {
-		int openSquare = 0;
-		int closeSquare = 0;
-		int openRound = 0;
-		int closeRound = 0;
-		int joker = 0;
-		if (hmap.containsKey('[')) {
-			openSquare = hmap.get('[');
-		}
-		if (hmap.containsKey(']')) {
-			closeSquare = hmap.get(']');
-		}
-		if (hmap.containsKey('(')) {
-			openRound = hmap.get('(');
-		}
-		if (hmap.containsKey(')')) {
-			closeRound = hmap.get(')');
-		}
-		if (hmap.containsKey('?')) {
-			joker = hmap.get('?');
-		}
-		int value = Math.abs(openSquare - closeSquare) + Math.abs(openRound - closeRound);
-		if (value == joker) {
-			return true;
-		}
-		if(joker > value && (joker - value) % 2 == 0) {
-			return true;
-		}
-		if (openSquare - closeSquare == 0 && openRound - closeRound == 0 && joker % 2 == 0) {
-			return true;
-		}
-		return false;
-	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "))?)?)?))?())()(??(?((())(?)?())((((()(?";
-		String s1 = "[(?][??[";
-		System.out.println(getNumberOfDigits(s));
-		System.out.println(getNumberOfDigits(s1));
+		int[] arr = { 10, 10, 10, 25, 30, 30 };
+		System.out.println("The frequencies of elements in array are: ");
+		findFrequencies(arr);
 	}
 }
