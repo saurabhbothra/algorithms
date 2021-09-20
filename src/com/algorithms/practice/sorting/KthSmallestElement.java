bb0ace557962503e11d79733b4282e2b54ac99c9
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-20 16:52:03 -0400

    kth smallest element in an array

--- src/com/algorithms/practice/sorting/KthSmallestElement.java
+++ src/com/algorithms/practice/sorting/KthSmallestElement.java
@@ -43,12 +43,54 @@ public class KthSmallestElement {
 		return i;
 	}
 
+	// solution using randomization.
+	public static int kthSmallest(int nums[], int k) {
+		// Your code here
+		int start = 0;
+		int end = nums.length - 1;
+		int result = -1;
+		while (start <= end) {
+			int randIndex = (int) ((Math.random() * (end + 1 - start)) + start);
+			int pivotIndex = partition(nums, start, end, randIndex);
+			if (pivotIndex == k - 1) {
+				result = nums[pivotIndex];
+				break;
+			} else if (pivotIndex > k - 1) {
+				end = pivotIndex - 1;
+			} else {
+				start = pivotIndex + 1;
+			}
+		}
+		return result;
+	}
+
+	private static int partition(int[] nums, int start, int end, int in) {
+		int temp = nums[end];
+		nums[end] = nums[in];
+		nums[in] = temp;
+		int pivotElement = nums[end];
+		int index = start - 1;
+		for (int i = start; i < end; i++) {
+			if (nums[i] < pivotElement) {
+				index++;
+				temp = nums[i];
+				nums[i] = nums[index];
+				nums[index] = temp;
+			}
+		}
+		index++;
+		temp = nums[index];
+		nums[index] = nums[end];
+		nums[end] = temp;
+		return index;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = { 30, 20, 5, 10, 8 };
 		int[] arr1 = { 10, 5, 30, 12 };
 		System.out.println("The kth smallest element in this array is: " + kSmallestElement(arr, 4));
-		System.out.println("The kth smallest element in this array is: " + kSmallestElement(arr1, 2));
+		System.out.println("The kth smallest element in this array is: " + kthSmallest(arr1, 2));
 	}
 
 }
