e415d8f6e7dbc943169d19cb0944cfdf0511d94d
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-24 00:12:05 -0400

    merge two sorted lists

--- src/com/algorithms/practice/linkedlist/MergeTwoSortedLists.java
+++ src/com/algorithms/practice/linkedlist/MergeTwoSortedLists.java
@@ -36,4 +36,48 @@ public class MergeTwoSortedLists {
 		return head;
 	}
 
+	// better iterative implementation.
+	public Node mergeTwoLists(Node l1, Node l2) {
+		Node curr1 = l1;
+		Node curr2 = l2;
+		Node head = new Node(-1);
+		Node tail = head;
+		while (curr1 != null && curr2 != null) {
+			if (curr1.data <= curr2.data) {
+				tail.next = curr1;
+				curr1 = curr1.next;
+			} else {
+				tail.next = curr2;
+				curr2 = curr2.next;
+			}
+			tail = tail.next;
+		}
+
+		if (curr1 == null) {
+			tail.next = curr2;
+		} else {
+			tail.next = curr1;
+		}
+		return head.next;
+	}
+
+	// recursive implementation.
+	public Node mergeTwoListsRec(Node l1, Node l2) {
+		if (l1 == null) {
+			return l2;
+		}
+
+		if (l2 == null) {
+			return l1;
+		}
+
+		if (l1.data <= l2.data) {
+			l1.next = mergeTwoListsRec(l1.next, l2);
+			return l1;
+		}
+
+		l2.next = mergeTwoListsRec(l1, l2.next);
+		return l2;
+	}
+
 }
