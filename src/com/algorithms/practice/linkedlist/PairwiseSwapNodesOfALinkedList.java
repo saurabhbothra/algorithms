c0808b7c5d7b87c234b443849b4b044e2a58ee22
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-23 23:58:44 -0400

    pairwise swap nodes of a linked list

--- src/com/algorithms/practice/linkedlist/PairwiseSwapNodesOfALinkedList.java
+++ src/com/algorithms/practice/linkedlist/PairwiseSwapNodesOfALinkedList.java
@@ -37,6 +37,45 @@ public class PairwiseSwapNodesOfALinkedList {
 		return newHead;
 	}
 
+	// iterative, better implementation.
+	public static Node swapPairs2(Node head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		Node tail = null;
+		Node curr = head;
+		head = null;
+		while (curr != null && curr.next != null) {
+			Node temp = curr.next.next;
+			if (head == null) {
+				head = curr.next;
+				tail = curr;
+				head.next = tail;
+			} else {
+				tail.next = curr.next;
+				tail.next.next = curr;
+				tail = curr;
+			}
+			curr = temp;
+		}
+
+		tail.next = curr;
+		return head;
+	}
+
+	// recursive solution.
+	public static Node swapPairsRec(Node head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		Node temp = head.next.next;
+		Node curr = head.next;
+		curr.next = head;
+		head.next = swapPairsRec(temp);
+		return curr;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Node head = LinkedList.insertAtBegin(null, 17);
