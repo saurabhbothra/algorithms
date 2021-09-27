6debb90437db905d06eee0bbec09294fac16094f
saurabhbothra <saurabhbothra29@gmail.com>
2021-09-27 14:00:06 -0400

    postorder traversal

--- src/com/algorithms/practice/tree/PostorderTraversal.java
+++ src/com/algorithms/practice/tree/PostorderTraversal.java
@@ -1,5 +1,10 @@
 package com.algorithms.practice.tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PostorderTraversal {
 
 	// Implement post order traversal of binary tree. (left subtree -> right subtree
@@ -13,5 +18,28 @@ public class PostorderTraversal {
 		postorderTraversal(root.left);
 		postorderTraversal(root.right);
 		System.out.print(root.data + " ");
+	}
+
+	// iterative solution.
+	public static List<Integer> postorderTraversalIter(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		if (root == null) {
+			return result;
+		}
+		ArrayDeque<TreeNode> st = new ArrayDeque<>();
+		st.push(root);
+		TreeNode curr = root;
+		while (!st.isEmpty()) {
+			TreeNode poppedNode = st.pop();
+			result.add(poppedNode.val);
+			if (poppedNode.left != null) {
+				st.push(poppedNode.left);
+			}
+			if (poppedNode.right != null) {
+				st.push(poppedNode.right);
+			}
+		}
+		Collections.reverse(result);
+		return result;
 	}
 }
