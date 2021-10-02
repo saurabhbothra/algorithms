8963ed6865a61dc68ded24ab85abeca33e5d7318
saurabhbothra <saurabhbothra29@gmail.com>
2021-10-02 01:32:30 -0400

    symmetric tree

--- src/com/algorithms/practice/general/SymmetricTree.java
+++ src/com/algorithms/practice/general/SymmetricTree.java
@@ -1,5 +1,7 @@
 package com.algorithms.practice.general;
 
+import java.util.ArrayDeque;
+
 import com.algorithms.practice.tree.TreeNode;
 
 // Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
@@ -25,5 +27,38 @@ public class SymmetricTree {
 		}
 
 		return isSymmetricRec(curr1.left, curr2.right) && isSymmetricRec(curr1.right, curr2.left);
+	}
+
+	// bfs solution.
+	public boolean isSymmetricBfs(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		return bfs(root, root);
+	}
+
+	private boolean bfs(TreeNode curr1, TreeNode curr2) {
+		ArrayDeque<TreeNode> q = new ArrayDeque<>();
+		TreeNode dummy = new TreeNode(-1);
+		q.offer(curr1);
+		q.offer(curr2);
+		while (!q.isEmpty()) {
+			TreeNode p1 = q.poll();
+			TreeNode p2 = q.poll();
+			if (p1 == dummy && p2 == dummy) {
+				continue;
+			}
+			if (p1 == dummy || p2 == dummy) {
+				return false;
+			}
+			if (p1.data != p2.data) {
+				return false;
+			}
+			q.offer((p1.left == null) ? dummy : p1.left);
+			q.offer((p2.right == null) ? dummy : p2.right);
+			q.offer((p1.right == null) ? dummy : p1.right);
+			q.offer((p2.left == null) ? dummy : p2.left);
+		}
+		return true;
 	}
 }
