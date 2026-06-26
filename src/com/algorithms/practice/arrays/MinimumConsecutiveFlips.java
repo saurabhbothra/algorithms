9cba6771e678cd3ce3774cebbd04a9db905986ad
Saurabh Bothra <saurabhbothra29@gmail.com>
2026-06-25 19:19:57 -0700

    Add minimum consecutive flips count for a binary array

--- src/com/algorithms/practice/arrays/MinimumConsecutiveFlips.java
+++ src/com/algorithms/practice/arrays/MinimumConsecutiveFlips.java
@@ -8,6 +8,28 @@ public class MinimumConsecutiveFlips {
 	// We need to find out minimum number of flips required to make all array
 	// elements same.
 
+    public static int minimumConsecutiveFlipsCount(int[] arr) {
+        int onesGroup = 0;
+        int zeroGroup = 0;
+        int curr = arr[0];
+        if (arr[0] == 0) {
+            zeroGroup++;
+        } else {
+            onesGroup++;
+        }
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] != curr) {
+                curr = arr[i];
+                if(arr[i] == 0) {
+                    zeroGroup++;
+                } else  {
+                    onesGroup++;
+                }
+            }
+        }
+        return Math.min(onesGroup, zeroGroup);
+    }
+
 	// efficient solution, in one traversal.
 	public static void minimumConsecutiveFlips(int[] arr) {
 		if (arr.length == 0) {
@@ -43,8 +65,16 @@ public class MinimumConsecutiveFlips {
 		int[] arr = { 1, 1, 0, 0, 0, 1 };
 		int[] arr1 = { 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1 };
 		int[] arr2 = { 0, 0, 1, 1, 0, 0, 1, 1, 0 };
+        int[] arr3 = { 1, 1, 1 };
+        int[] arr4 = { 0, 1 };
 		System.out.println("The minimum consecutive flips for this array are: ");
 		minimumConsecutiveFlips(arr);
+        System.out.println();
+        System.out.println("The minimum consecutive flips count for this array are: " +  minimumConsecutiveFlipsCount(arr));
+        System.out.println("The minimum consecutive flips count for this array are: " +  minimumConsecutiveFlipsCount(arr1));
+        System.out.println("The minimum consecutive flips count for this array are: " +  minimumConsecutiveFlipsCount(arr2));
+        System.out.println("The minimum consecutive flips count for this array are: " +  minimumConsecutiveFlipsCount(arr3));
+        System.out.println("The minimum consecutive flips count for this array are: " +  minimumConsecutiveFlipsCount(arr4));
 	}
 
 }
