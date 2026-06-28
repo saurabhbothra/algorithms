ac672a8e6163bac255db88566c4d5519c905a9be
Saurabh Bothra <saurabhbothra29@gmail.com>
2026-06-28 13:33:34 -0700

    Add search in unknown sorted array problem

---
+++ src/com/algorithms/practice/binarysearch/SearchInSortedArrayUnknownSize.java
@@ -0,0 +1,64 @@
+package com.algorithms.practice.binarysearch;
+
+/*
+ LC: 702: This is an interactive problem.
+
+You have a sorted array of unique elements and an unknown size. You do not have an access to the array but you can use the ArrayReader interface to access it. You can call ArrayReader.get(i) that:
+
+returns the value at the ith index (0-indexed) of the secret array (i.e., secret[i]), or
+returns 231 - 1 if the i is out of the boundary of the array.
+You are also given an integer target.
+
+Return the index k of the hidden array where secret[k] == target or return -1 otherwise.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: secret = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in secret and its index is 4.
+Example 2:
+
+Input: secret = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in secret so return -1.
+
+
+Constraints:
+
+- 1 <= secret.length <= 104
+- -104 <= secret[i], target <= 104
+- secret is sorted in a strictly increasing order.
+ */
+
+interface ArrayReader {
+    public int get(int index);
+}
+
+public class SearchInSortedArrayUnknownSize {
+
+    public int search(ArrayReader reader, int target) {
+        int valAtFirst = reader.get(0);
+        int negativeElements = 0;
+        if (valAtFirst < 0) {
+            negativeElements = -1 * valAtFirst;
+        }
+        int end = negativeElements + target;
+        int start = 0;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(reader.get(mid) == target) {
+                return mid;
+            }
+            if (reader.get(mid) > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
